dce5bf5c215b43f461bc7fdc324d8f12e5ad6c3a
Peter Fesz-Nguyen <pfesznguyen@gmail.com>
2017-10-23 17:03:07 -0700

    minor fix

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var decache = require('decache')
 var myModule = require('./mymodule')
 
-decache('./mymodule')
 
 console.log('cache keys: ')
 console.log(Object.keys(require.cache))
 
+decache('./mymodule')
 console.log('decaching...')
 
 console.log('cache keys: ')
